af2ddbde4e2b968d1ac6689ec5cf332633770b4b
David whyte <kingstonwhyte87@gmail.com>
2019-05-17 18:04:47 +0100

    Initial Commit

---
+++ VineDb/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  VineDb
+//
+//  Created by David Whyte-John on 17/05/2019.
+//  Copyright © 2019 David Whyte-John. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
